55b5f3408c04b48a756612440fa0fe15cf099430
Oleksandr Herbut <berkyt96@icloud.com>
2025-02-28 20:39:01 +0100

    Main parent file

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
+// import { ThemeProvider } from './services/ThemeContext.tsx';
+import "./i18n";
+import { BrowserRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <ThemeProvider> */}
+    <Router>
+      <Provider store={store}>     
+        <App />     
+      </Provider>
+    </Router>
+    {/* </ThemeProvider> */}
+  </StrictMode>
+);
